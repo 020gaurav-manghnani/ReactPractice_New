6c07365697b4113653df6c00cba246f339095ac0
Gaurav Manghnani <Gaurav.Manghnani@carelon.com>
2024-08-28 01:29:29 +0530

    password Generator

---
+++ IntoJS/passwordGenerator.js
@@ -0,0 +1,5 @@
+// create a basic str variable and store whole cap and small alphabet into it
+// create a generate button which when clicked.. will get a part of string 
+// and place it inside the input field.
+
+
