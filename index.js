254944e42301326d8a5171b838372039c0bfc0a7
Gaurav Manghnani <Gaurav.Manghnani@carelon.com>
2024-08-27 15:37:40 +0530

    04bgChanger

---
+++ index.js
@@ -0,0 +1,7 @@
+const selectContainer = document.querySelector('.container');
+const button = document.querySelector('.btn');
+const body = document.querySelector('body')
+
+function colorChanger(color){
+    body.style.backgroundColor=color
+}
